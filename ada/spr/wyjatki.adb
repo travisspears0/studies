2dbb6e95fe27eb0e8be16f05f62f17515637d915
karol_bisztyga <karolbisztyga@gmail.com>
2016-11-03 10:44:39 +0100

    asd

--- ada/spr/wyjatki.adb
+++ ada/spr/wyjatki.adb
@@ -6,13 +6,18 @@ procedure wyjatki is
 	function pow(a:integer;n:integer) return integer is
 		result:integer := 1;
 	begin
+		if n < 0 then raise numeric_error; end if;
 		for i in 1..n loop
 			result := result*a;
 		end loop;
 		return result;
 	end;
 begin
-	put_line(silnia(5)'img);
-	put_line(pow(2,5)'img);
+	begin
+		put_line(silnia(5)'img);
+		put_line(pow(3,-2)'img);
+	exception
+		when numeric_error => put_line("potega nie moze byc mniejsza od zera");
+	end;
 end wyjatki;
 
