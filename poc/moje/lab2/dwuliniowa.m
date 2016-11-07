29edd932442c5e2c357802eca7f808ebabb0eabf
karol_bisztyga <karolbisztyga@gmail.com>
2016-11-07 12:17:11 +0100

    asd

--- poc/moje/lab2/dwuliniowa.m
+++ poc/moje/lab2/dwuliniowa.m
@@ -2,47 +2,45 @@
 clearvars;
 close all;
 clc;
-%vars
-lena = imread('archiwum/lena.bmp');
-lena256 = imresize(lena,.5);
-lena128 = imresize(lena,.25);
-lena64 = imresize(lena,.125);
 %interpolacji dwuliniowa
 
 parrot = imread('archiwum/parrot.bmp');
-scale = 2.5;
+scale = 7.5;
 
 xscale = scale;
 yscale = scale;
 
-[y,x] = size(parrot);
+[x,y] = size(parrot);
 nx = round(x*xscale);
 ny = round(y*yscale);
 
 xstep = x/nx;
 ystep = y/ny;
-parrotdouble = double(parrot);
 
 scaled = uint8(zeros(ny,nx));
-for yi=0:(ny-1)
-    yii = floor(yi*ystep);
-    for xi=0:(nx-1)
-        xii = floor(xi*xstep);
-        adx = xii+1;
-        bcx = xii+2;
-        aby = yii+1;
-        cdy = yii+2;
-        if (adx > x) adx = x; end
-        if (bcx > x) bcx = x; end
-        if (aby > y) aby = y; end
-        if (cdy > y) cdy = y; end
-        fA = parrotdouble((aby), (adx));
-        fB = parrotdouble((aby), (bcx));
-        fC = parrotdouble((cdy), (bcx));
-        fD = parrotdouble((cdy), (adx));
+
+parrotdouble = double(parrot);
+
+for i=0:(ny-1)
+    xi = i*xstep;
+    if(xi>x-2) xi=x-2; end;
+    if(xi<1) xi=1; end;
+    fxi = floor(xi);
+    for j=0:(nx-1)
+        yi = j*ystep;
+        if(yi>y-2) yi=y-2; end;
+        if(yi<1) yi=1; end;
+        fyi = floor(yi);
         
-        fABCD = [1-yi yi]*[fA fD;fB fC]*[1-xi;xi];
-        scaled(yi+1, xi+1) = fABCD;
+        a = parrotdouble(fxi+1,fyi+1);
+        b = parrotdouble(fxi+2,fyi+1);
+        c = parrotdouble(fxi+2,fyi+2);
+        d = parrotdouble(fxi+1,fyi+2);
+        
+        di = xi-fxi;
+        dj = yi-fyi;
+        
+        scaled(i+1,j+1)=a*(1-di)*(1-dj) + b*di*(1-dj) + c*di*dj + d*(1-di)*dj;
     end
 end
 figure(1);
