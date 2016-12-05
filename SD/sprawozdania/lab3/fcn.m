b362ab8a9f9489354baa179ed7cf3abae19621d9
karol_bisztyga <karolbisztyga@gmail.com>
2016-12-05 19:12:47 +0100

    sd

---
+++ SD/sprawozdania/lab3/fcn.m
@@ -0,0 +1,4 @@
+function [ fx ] = fcn( x )
+    fx = 3*x^3+4*x^2+5;
+end
+
--- SD/sprawozdania/lab3/script4.m
+++ SD/sprawozdania/lab3/script4.m
@@ -2,18 +2,44 @@
 clearvars;
 close all;
 clc;
-model = 'lab3_model_4';
 
-%punkty poczatkowe
-x0=1;
+%punkt poczatkowy
+x0=-1;
 %dokladnosc
 e=.1;
+%krance przedzialu 
+a = -2.5;
+b = -.5;
+%maksymalna ilosc iteracji
+maxiter = 20;
+i=0;
+oldx = x0;
+oldf = fcn(x0);
+while(i<maxiter)
+    %f(xn)
+    f = fcn(oldx);
+    %f'(xn)
+    fp = diff(f);
+    %x(n+1)
+    x= oldx-(f/fp);
+    display(sprintf('iteracja %d, x: %d',i,x));
+    %wrunki wyjscia z petli
+    if(abs(fcn(x)) <= e)
+        display('war1');
+        break;
+    end;
+    if(abs(oldf-fcn(x))<=e)
+        display('war2');
+        break;
+    end;
+    i=i+1;
+    oldx = x;
+    oldf = f;
+end
 
-sim(model);
-figure;
-plot(simout);
-%ustawienia wykresu
-grid on;
-xlabel('t');
-ylabel('x(t)');
-title(sprintf('wykres...'));
+display(sprintf('wynik: %d',x));
+display(sprintf('funkcja od wyliczonego x: %d',cos(x)));
+
+display(sprintf('iteracji: %d',i));
+
+
